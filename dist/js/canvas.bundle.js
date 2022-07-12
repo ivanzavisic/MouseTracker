dece0986f2f7bc76c71b53939233c3f0bf127619
ivan <ivan.zavisic@gmail.com>
2022-07-12 18:26:33 +0200

    feat: add visual mouse tracker

---
+++ dist/js/canvas.bundle.js
@@ -0,0 +1,217 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/canvas.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/canvas.js":
+/*!**************************!*\
+  !*** ./src/js/canvas.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+var mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2
+}; // Event Listeners
+
+addEventListener('mousemove', function (event) {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
+addEventListener('resize', function () {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  init();
+}); //Objects
+
+var Particle = /*#__PURE__*/function () {
+  function Particle(x, y, radius, color, velocity) {
+    _classCallCheck(this, Particle);
+
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.ttl = 1000;
+  }
+
+  _createClass(Particle, [{
+    key: "draw",
+    value: function draw() {
+      c.beginPath();
+      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      c.fillStyle = this.color;
+      c.fill();
+      c.closePath();
+    }
+  }, {
+    key: "update",
+    value: function update() {
+      this.draw();
+      this.x += this.velocity.x;
+      this.y += this.velocity.y;
+      this.ttl--;
+    }
+  }]);
+
+  return Particle;
+}(); //Implementation
+
+
+var particles;
+
+function init() {
+  particles = [];
+  var radius = 100;
+
+  for (var i = 0; i < 400; i++) {
+    //full circle = pi * 2 * radians
+    var radian = Math.PI * 2 / 30;
+    Math.cos();
+    var x = canvas.width / 2;
+    var y = canvas.height / 2;
+    particles.push(new Particle(x, y, 5, 'hsl(0, 50%, 50%)', {
+      x: Math.cos(radian * i) * 2,
+      y: Math.sin(radian * i) * 2
+    }));
+  }
+}
+
+var hue = 0;
+var hueRadians = 0;
+
+function generateRing() {
+  setTimeout(generateRing, 200);
+  hue = Math.sin(hueRadians);
+
+  for (var i = 0; i < 30; i++) {
+    //full circle = pi * 2 * radians
+    var radian = Math.PI * 2 / 30;
+    var x = mouse.x;
+    var y = mouse.y;
+    particles.push(new Particle(x, y, 5, "hsl(".concat(Math.abs(hue * 360), ", 50%, 50%)"), {
+      x: Math.cos(radian * i),
+      y: Math.sin(radian * i)
+    }));
+  }
+
+  hueRadians += 0.01;
+} //animation loop
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillStyle = "rgba(0, 0, 0, 1)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  particles.forEach(function (particle, i) {
+    if (particles.ttl < 0) {
+      particles.splice(i, 1);
+    } else {
+      particle.update();
+    }
+  });
+}
+
+init();
+animate();
+generateRing();
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=canvas.bundle.js.map
---
+++ src/js/canvas.js
@@ -0,0 +1,109 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const mouse = {
+    x: innerWidth / 2,
+    y: innerHeight / 2
+}
+
+// Event Listeners
+addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+})
+
+addEventListener('resize', () => {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+
+    init();
+})
+
+
+//Objects
+class Particle {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+        this.ttl = 1000;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+        c.closePath();
+    }
+
+    update() {
+        this.draw();
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        this.ttl--;
+    }
+}
+
+//Implementation
+let particles;
+function init() {
+    particles = [];
+    const radius = 100;
+
+    for (let i = 0; i < 400; i++) {
+        //full circle = pi * 2 * radians
+        const radian = (Math.PI * 2)/30;
+        Math.cos()
+        const x = canvas.width/2;
+        const y = canvas.height/2;
+        particles.push(new Particle(
+            x, y, 5, 'hsl(0, 50%, 50%)', {
+                x: Math.cos(radian * i)*2,
+                y: Math.sin(radian * i)*2
+            }))
+    }
+}
+let hue = 0;
+let hueRadians = 0;
+
+function generateRing() {
+    setTimeout(generateRing, 200);
+    hue = Math.sin(hueRadians);
+    for (let i = 0; i < 30; i++) {
+        //full circle = pi * 2 * radians
+        const radian = (Math.PI * 2)/30;
+        const x = mouse.x;
+        const y = mouse.y;
+        particles.push(new Particle(
+            x, y, 5, `hsl(${Math.abs(hue*360)}, 50%, 50%)`, {
+                x: Math.cos(radian * i),
+                y: Math.sin(radian * i)
+            }))
+    }
+    hueRadians += 0.01;
+}
+
+//animation loop
+function animate() {
+    requestAnimationFrame(animate);
+    c.fillStyle = "rgba(0, 0, 0, 1)"
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach((particle, i) => {
+        if (particles.ttl < 0) {
+            particles.splice(i, 1);
+        } else {
+            particle.update();
+        }
+    })
+}
+
+init();
+animate();
+generateRing();
